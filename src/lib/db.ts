037a6f4381d83b01cf7405b7a43efe5e75d1aadf
Levan Ilashvili <levan@ilashvili.me>
2025-09-28 17:53:07 +0530

    lol :D

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -7,7 +7,7 @@ const globalForPrisma = globalThis as unknown as {
 export const prisma = globalForPrisma.prisma ?? new PrismaClient({
   datasources: {
     db: {
-      url: process.env.DATABASE_URL || "postgres://postgres:30b9a9b6f3fc90e0b6ec@157.173.115.149:9218/memewarp?sslmode=disable"
+      url: process.env.DATABASE_URL
     }
   }
 })
